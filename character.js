3eb9e23d7a8f1aac94b35125815d435e55ddddb5
aldwin <olano.aa@gmail.com>
2026-01-26 19:41:36 -0700

    feat: rename Sir Valor.png to Sir_Valor.png and added array of character objects

---
+++ character.js
@@ -0,0 +1,74 @@
+const character = [
+  {
+    name: "Bravakar",
+    health: 150,
+    attack: 20,
+    defense: 15,
+    image: "/assets/images/characters/Bravakar.png",
+    description: "A strong melee fighter with high defense."
+  },
+  {
+    name: "Gorblin",
+    health: 120,
+    attack: 25,
+    defense: 10,
+    image: "/assets/images/characters/Gorblin.png",
+    description: "A cunning rogue with high agility."
+  },
+  {
+    name: "Eldramir",
+    health: 100,
+    attack: 30,
+    defense: 20,
+    image: "/assets/images/characters/Eldramir.png",
+    description: "A wise mage with powerful spells."
+  },
+  {
+    name: "Moonclaw",
+    health: 110,
+    attack: 20,
+    defense: 15,
+    image: "/assets/images/characters/Moonclaw.png",
+    description: "A swift rogue with high agility."
+  },
+  {
+    name: "Gorunox",
+    health: 130,
+    attack: 25,
+    defense: 20,
+    image: "/assets/images/characters/Gorunox.png",
+    description: "A fierce warrior with unmatched strength."
+  },
+  {
+    name: "Steamforge",
+    health: 140,
+    attack: 30,
+    defense: 25,
+    image: "/assets/images/characters/Steamforge.png",
+    description: "A mechanical genius with powerful gadgets."
+  },
+  {
+    name: "Sir Valor",
+    health: 160,
+    attack: 35,
+    defense: 30,
+    image: "/assets/images/characters/Sir_Valor.png",
+    description: "A noble knight with exceptional combat skills."
+  },
+  {
+    name: "Galaxion",
+    health: 170,
+    attack: 40,
+    defense: 35,
+    image: "/assets/images/characters/Galaxion.png",
+    description: "A cosmic being with extraordinary powers."
+  },
+  {
+    name: "Darkblade",
+    health: 180,
+    attack: 45,
+    defense: 40,
+    image: "/assets/images/characters/Darkblade.png",
+    description: "A shadowy assassin with lethal precision."
+  },
+]
