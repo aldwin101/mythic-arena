0dfc1a3d0537b2296e810a425259a969f410875b
aldwin <olano.aa@gmail.com>
2026-01-30 23:24:53 -0700

    feat: add click event to show selected character image and description

--- js/interface.js
+++ js/interface.js
@@ -1,25 +1,37 @@
-// import the function that loads character data
+// call the function so characters show on page load
+displayCharacters();
+
+// import the function that gets character data from the JSON file
 import { loadCharactersData } from "./charactersData.js";
 
 // function to display characters on the page
-export async function displayCharacters() {
-  const data = await loadCharactersData();
-  const characters = data.characters;
-  const roster = document.querySelector('.character-roster');
+export async function displayCharacters() { // async because it waits for data to load
+  const data = await loadCharactersData(); // wait for character data to be loaded
+  const characters = data.characters; // extract characters array from the data
+  const roster = document.querySelector('.character-roster'); // select the roster container
   
-  roster.innerHTML = '';
+  roster.innerHTML = ''; // clear existing content before adding new characters
 
+  // loop through each character and create HTML elements
   characters.forEach((character, index)=> {
     const figure = document.createElement('figure');
     figure.className = 'character-container';
 
     const dynamicNumber = index +1;
-    figure.innerHTML = `<img class="character-image char${dynamicNumber}" src="${character.image}" alt="${character.name}" >
-    <figcaption> ${character.name} - ${character.description}
-    </figcaption>`;
+    figure.innerHTML = `<img class="character-image char${dynamicNumber}" src="${character.image}" alt="${character.name}" >`;
 
-    roster.appendChild(figure);
+    roster.appendChild(figure); // add the figure element to the roster
+    
+    // add click event listener to each character image
+    figure.addEventListener('click', () => {
+      const selectedImage = document.querySelector('.selected-character-image img');
+
+      const descriptionBox = document.querySelector('.description-box');
+
+      selectedImage.src = character.image;
+      selectedImage.alt = character.name;
+
+      descriptionBox.innerHTML = `<h2>${character.name}</h2><p>${character.description}</p>`;
+    })
   })
 }
-
-displayCharacters();
