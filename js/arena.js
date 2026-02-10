4a0dd8a48b30fb6cf660acdaca4f30312a484e17
aldwin <olano.aa@gmail.com>
2026-02-09 22:29:26 -0700

    style(arena): center player image horizontally

--- js/arena.js
+++ js/arena.js
@@ -5,8 +5,8 @@ const savedCharacter = JSON.parse(
 document.querySelector('.player-name').textContent =
   savedCharacter.name;
 
-document.querySelector('.player-img').src =
-  savedCharacter.image;
-
 document.querySelector('.player-health').value =
   savedCharacter.stats.health;
+
+document.querySelector('.player-img').src =
+  savedCharacter.image;
