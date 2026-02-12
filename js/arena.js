91d1ed3e515cf1b25f24a1f1268a3874f5ea1d3a
aldwin <olano.aa@gmail.com>
2026-02-11 21:12:46 -0700

    feat(arena): added random enemy with health, name and save it using localStorage

--- js/arena.js
+++ js/arena.js
@@ -10,3 +10,36 @@ document.querySelector('.player-health').value =
 
 document.querySelector('.player-img').src =
   savedCharacter.image;
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+  const response = await fetch('./characters.json'); 
+  const data = await response.json(); // Load character data from JSON file
+  const characters = data.characters; // Access the characters array from the loaded data
+
+  // Check if enemy already saved
+  let savedEnemy = JSON.parse(localStorage.getItem("enemy"));
+
+  // If not, create random enemy
+  if (!savedEnemy) {
+    const randomEnemy =
+      characters[Math.floor(Math.random() * characters.length)];
+
+    localStorage.setItem("enemy", JSON.stringify(randomEnemy));
+    savedEnemy = randomEnemy;
+  }
+
+  // Display enemy
+  const enemyHealth = document.querySelector(".enemy-health");
+
+  document.querySelector(".enemy-name").textContent =
+    savedEnemy.name;
+
+  document.querySelector(".enemy-img").src =
+    savedEnemy.image;
+
+  enemyHealth.max = savedEnemy.stats.health;
+  enemyHealth.value = savedEnemy.stats.health;
+
+});
